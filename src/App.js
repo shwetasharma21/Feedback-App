ad04cb27f618d8456c90e493a8a498f5105389ca
Shweta Sharma <sshweta437@gmail.com>
2023-01-13 08:59:27 +0530

    Delete Functionalty Changes

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import FeedbackList from "./components/FeedbackList";
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData); // App level state
+	const deleteFeedback = (id) => {
+		if (window.confirm("Are you sure you want to delete?")) {
+			setFeedback(feedback.filter((item) => item.id !== id));
+		}
+	};
 
 	return (
 		<>
 			<Header />
 			<div className="container">
-				<FeedbackList feedback={feedback} />
+				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
 			</div>
 		</>
 	);
